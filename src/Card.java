5aa1885373df6c6fda12f611986fcf26dbebb8c2
owolfhu1 <owolfhu1@gmail.com>
2017-09-19 20:08:35 -0400

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -12,16 +12,8 @@ public class Card {
         return suit;
     }
 
-    public void setSuit(String suit) {
-        this.suit = suit;
-    }
-
     public String getValue() {
         return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
     }
 
 }
